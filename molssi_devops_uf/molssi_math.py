2278e75dfc149f00424fc4470fe139731a20e59b
Pilar Buteler <mpbuteler@gmail.com>
2019-04-05 15:17:43 -0400

    add error to molssi_math

--- molssi_devops_uf/molssi_math.py
+++ molssi_devops_uf/molssi_math.py
@@ -21,7 +21,17 @@ def mean(num_list):
     mean_list : float
         The mean of the list
     """
-    mean_list = sum(num_list) / len(num_list)
+    # check if input is type list
+    if not isinstance(num_list, list):
+        raise TypeError('Invalid input %s - Input must be type list' %(num_list))
+
+    # check that list is not empty
+    if len(num_list) == []:
+        raise ValueError('cannot calculate mean of empty list.')
+    try:
+       mean_list = sum(num_list)/len(num_list)
+    except TypeError:
+           raise TypeError('Cannot calculate mean of a list - all list elements must be numeric')
 
     return mean_list
 
